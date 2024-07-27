ec4c3182303dfc2dc3732ece652df9d6129f8349
Kumar Rohit <rohit.kumar01091997@gmail.com>
2024-07-27 22:57:35 +0530

    updated stopwatch css

--- src/Projects/Stopwatch/Stopwatch.jsx
+++ src/Projects/Stopwatch/Stopwatch.jsx
@@ -1,16 +1,27 @@
 import * as React from 'react';
 import useTimer from './useTimer';
+import './style.css';
 
 const Stopwatch = () => {
 
     const {time, startTimeCount, pauseTimeCount, resetTimeCount} = useTimer();
 
     return(
-        <div>
-            <p>{time.hour<10?`0${time.hour}`:time.hour}:{time.min<10?`0${time.min}`:time.min}:{time.sec<10?`0${time.sec}`:time.sec}:{time.milliSec<10?`00${time.milliSec}`:time.milliSec<100?`0${time.milliSec}`:time.milliSec}</p>
-            <button onClick={startTimeCount}>Start</button>
-            <button onClick={pauseTimeCount}>Pause</button>
-            <button onClick={resetTimeCount}>Reset</button>
+        <div id="stopwatch">
+            <p className='counter'>
+                <span><sup>HH</sup>{time.hour<10?`0${time.hour}`:time.hour}</span>
+                :
+                <span><sup>mm</sup>{time.min<10?`0${time.min}`:time.min}</span>
+                :
+                <span><sup>ss</sup>{time.sec<10?`0${time.sec}`:time.sec}</span>
+                .
+                <span><sup>SSS</sup>{time.milliSec<10?`00${time.milliSec}`:time.milliSec<100?`0${time.milliSec}`:time.milliSec}</span>
+            </p>
+            <div className='counterButtons'>
+                <button className="btn btn-primary" onClick={startTimeCount}>Start</button>
+                <button className="btn btn-primary" onClick={pauseTimeCount}>Pause</button>
+                <button className="btn btn-secondary" onClick={resetTimeCount}>Reset</button>  
+            </div>
         </div>
     )
 }
