e0159569e2279492fd71055b74d3ca15f38cd742
Kumar Rohit <rohit.kumar01091997@gmail.com>
2024-07-29 00:57:37 +0530

    updated back button

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import { ArrowLeftCircle } from 'react-bootstrap-icons';
 
 const Header = (props) => {
     return(
         <div id="header">
-            <div id="main-title">React Practice Projects</div>
+            <div id="heading">
+                {props.selectedProject && <ArrowLeftCircle onClick={()=>props.setSelectedProject(null)} id="back-btn"/>}
+                <div id="main-title">React Practice Projects</div>
+            </div>
             {props.selectedProject?
             <>
                 <div id="project-title">{props.selectedProject.Title}</div>
                 <div id="header-btns">
                     {/* <div id="project-about-btn">About</div> */}
-                    <div id="back-btn" onClick={()=>props.setSelectedProject(null)}>Back</div>
+                    {/* <div id="back-btn" onClick={()=>props.setSelectedProject(null)}>Back</div> */}
                 </div>
             </>
             :''
